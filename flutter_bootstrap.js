d67ef27bcf3186f30b5f078adfcf9b012392bd81
onjobes <eslam1984m6@gmail.com>
2026-06-13 04:41:58 +0300

    Update contacts and web conversations

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "823633530" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "50215428" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
