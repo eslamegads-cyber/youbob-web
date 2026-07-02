0adb9cabfc0022b30a9a39b7d67862b7da23b56b
onjobes <eslam1984m6@gmail.com>
2026-07-02 19:14:05 +0300

    Deploy profile map and voice room updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2298440337" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4073884503" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
